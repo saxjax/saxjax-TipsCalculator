da59c89e9035f7a8a00031d42d0fdf2d4f7fb879
saxjax <saxjax@saxjax.dk>
2022-07-18 14:48:20 +0300

    outlets and actions hooked up

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CalculatorViewController.swift
+//  Tipsy
+//
+//  Created by Jakob Skov Søndergård on 18/07/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: Any) {
+    }
+
+    @IBAction func stapperValueChanged(_ sender: UIStepper) {
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
